87b415c13686de8ee0daefc82d4644a32f1e7940
k-net <fernandooliveira@knet.ca>
2013-03-22 11:37:16 -0300

    Update version.php

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
  */
 
 
-$plugin->version = 2013032101;
+$plugin->version = 2013032201;
 $plugin->requires = 2012062500; // 2.3 
 $plugin->maturity = MATURITY_RC;
 $plugin->component = 'format_fntabs';
